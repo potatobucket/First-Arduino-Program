c1e38452df35145494b957fa48b01fe754862ca9
potatobucket <104957727+potatobucket@users.noreply.github.com>
2024-08-26 18:46:35 -0500

    Wrote program to turn the LED on
    It's the LED that comes on the board. For testing, I reckon.

---
+++ Turn-Board-LED-on/Turn-Board-LED-on.ino
@@ -0,0 +1,9 @@
+void setup() {
+  // sets pin 13 to OUTPUT
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  //sets pin 13's output to HIGH thus turning on the LED on the board
+  digitalWrite(13, HIGH);
+}
